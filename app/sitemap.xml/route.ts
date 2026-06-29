7ead742b3369a23e5201c01fecf74a5785f957b0
ThiagoRdrgs00 <thiagorodrigues221100@gmail.com>
2026-06-29 08:51:28 -0300

    update 5: faltou a routes

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server"
+
+const BASE_URL = "https://alfamed-page.vercel.app"
+
+export function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${BASE_URL}/</loc>
+    <lastmod>2026-06-29</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`
+
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=86400",
+    },
+  })
+}
